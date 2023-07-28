14f90b62cf3589a921646d58f3c7ce4a5e64d410
deepak.dhormare <deepak.dhormare@coupa.com>
2023-07-28 21:28:12 +0530

    completed look_test move

--- analytics_tests/step_one.spec.js
+++ analytics_tests/step_one.spec.js
@@ -12,26 +12,35 @@ await page.locator('body').click();
 await page.getByLabel('Password').click();
 await page.getByLabel('Password').fill('Temp@1234');
 await page.getByRole('button', { name: 'Sign In' }).click();
+await page.pause()
 await page.goto('https://clone-groupon-incremental-30.coupadev.com/analytics');
 await page.getByRole('link', { name: 'Create New Report' }).click();
-await page.goto('https://clone-groupon-incremental-30.coupadev.com/analytics/embed/explore/coupa_analytics%7Cinvoices');
-await page.frameLocator('#analytics_iframe').getByPlaceholder('Start typing to search').click();
-await page.frameLocator('#analytics_iframe').getByPlaceholder('Start typing to search').fill('invoice reporting total');
-await page.frameLocator('#analytics_iframe').getByLabel('Line Level Measures').getByText('Invoice Line Reporting Total', { exact: true }).click();
-await page.frameLocator('#analytics_iframe').getByRole('button', { name: 'Clear Field' }).click();
-await page.frameLocator('#analytics_iframe').getByLabel('All Fields').getByText('Invoices', { exact: true }).click();
-await page.frameLocator('#analytics_iframe').getByText('Line Level Measures').click();
+await page.getByRole('link', { name: 'Invoices' }).click();
+
+await page.frameLocator('#analytics_iframe').getByPlaceholder('Start typing to search').isVisible();
+await page.frameLocator('#analytics_iframe').getByRole('treeitem', { name: 'Invoices', exact: true }).locator('svg').click();
+await page.frameLocator('#analytics_iframe').getByRole('treeitem', { name: 'Line Level Measures' }).locator('svg').click();
 await page.frameLocator('#analytics_iframe').getByLabel('Line Level Measures').getByText('Invoice Line Reporting Total', { exact: true }).click();
 await page.frameLocator('#analytics_iframe').getByLabel('Run', { exact: true }).click();
-
 await page.frameLocator('#analytics_iframe').getByRole('button', { name: 'Explore actions' }).click();
 await page.frameLocator('#analytics_iframe').getByRole('menuitem', { name: 'Save...' }).click();
 await page.frameLocator('#analytics_iframe').getByRole('menuitem', { name: 'As a Look' }).click();
 await page.frameLocator('#analytics_iframe').locator('#qr-look-modal-title-field').click();
-await page.frameLocator('#analytics_iframe').locator('#qr-look-modal-title-field').fill('look_test_123');
+await page.frameLocator('#analytics_iframe').locator('#qr-look-modal-title-field').fill('look_test_12366');
 await page.frameLocator('#analytics_iframe').locator('lk-file-navigator').getByRole('button', { name: 'My folder' }).click();
 await page.frameLocator('#analytics_iframe').locator('lk-file-navigator').getByRole('button', { name: 'My folder' }).click();
 await page.frameLocator('#analytics_iframe').getByRole('button', { name: 'Save & View Look' }).click();
+await page.goto('https://clone-groupon-incremental-30.coupadev.com/analytics');
+await page.getByRole('link', { name: 'redwood analytics-admin' }).click();
+await page.locator("div.spend-analysis-folder-contents",{hasText:"Looks"}).locator("tr",{hasText:"look_test_12366"}).locator(".enable-bootstrap").isVisible()
+await page.locator("div.spend-analysis-folder-contents",{hasText:"Looks"}).locator("tr",{hasText:"look_test_12366"}).locator(".enable-bootstrap").click();
+await page.locator("div.spend-analysis-folder-contents",{hasText:"Looks"}).locator("tr",{hasText:"look_test_12366"}).locator(".enable-bootstrap").locator(".dropdown-menu").locator("li",{hasText:"Move"}).isVisible()
+
+await page.locator("div.spend-analysis-folder-contents",{hasText:"Looks"}).locator("tr",{hasText:"look_test_12366"}).locator(".enable-bootstrap").locator(".dropdown-menu").locator("li",{hasText:"Move"}).click()
+await page.locator(".ui-dialog-content.ui-widget-content").locator("select").selectOption({label:"Clone Groupon Incremental 30"})
+await page.getByRole('button', { name: 'Submit' }).click()
+
+
 
 
 
