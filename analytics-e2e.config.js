b5e74ecebdc9cbfae2276c879e08dda27e3e34d6
deepak.dhormare <deepak.dhormare@coupa.com>
2023-09-05 00:35:00 +0530

    full proof made this test cases

--- analytics-e2e.config.js
+++ analytics-e2e.config.js
@@ -4,14 +4,14 @@ const { defineConfig, devices } = require('@playwright/test');
 module.exports = defineConfig({
   // which tests to run
   
-  timeout: 50 * 1000,
+  timeout: 100 * 1000,
   expect: {
-    timeout: 50 * 1000,
+    timeout: 100 * 1000,
   },
   use:{
     headless: true,
     viewport: {width:1280, height: 720},
-    actionTimeout: 15000,
+    actionTimeout: 150000,
     video: "retain-on-failure",
     screenshot: "only-on-failure"
 },
@@ -23,7 +23,19 @@ module.exports = defineConfig({
         browserName: 'chromium',
         headless: false,
       },
-    } 
+    } ,    {
+      name: 'firefox',
+      use: {
+        browserName: 'firefox',
+        headless: true,
+      },
+    } , {
+      name: 'webkit',
+      use: {
+        browserName: 'webkit',
+        headless: false,
+      },
+    }
   ],
    // Number of parallel workers (adjust as needed)
 });
